578c86cdbd47ec62c44e8d62d3b91088f16b4547
Ecarateu <ecarateu@gmail.com>
2020-11-10 22:45:50 -0300

    implementação de contador

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,29 @@
-function Home(){
-    return <div>Ta funcionandooo ihuuuuuu</div>
+import { useState } from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h2>Conta ai, quanto vale um abraço teu</h2>
+            <Contador />
+            
+        </div>
+    )
 }
-export default Home
+
+function Contador() {
+    const [contador,setContador] = useState(1);
+
+    function adicionarContador() {
+        setContador(contador + 1);
+    }
+
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adicionar</button>
+        </div>
+    )
+    return <h1>Home</h1>
+}
+
+export default Home 
